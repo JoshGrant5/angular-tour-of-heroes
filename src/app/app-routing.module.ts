89576ffba142dad01a3bd4395951efc0fa62d1fd
JoshGrant5 <joshgg@icloud.com>
2020-12-21 14:01:59 -0800

    Generate dashboard component and add route navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
-const routes: Routes = [];
+// A typical Angular Route has two properties:
+
+// path: a string that matches the URL in the browser address bar.
+// component: the component that the router should create when navigating to this route.
+
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
